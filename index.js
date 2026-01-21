2d8ee86dd4bed63a1d5b1c6a4d4b17d7a6d588dd
Paweł Gr <pawelgrzybek2@wp.pl>
2026-01-21 16:51:56 +0100

    Add auth with google strategy and mongo

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+require("./models/User");
+
+require("./services/passport");
+
+mongoose.connect(require("./config/keys").mongoURI);
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [require("./config/keys").cookieKey],
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
