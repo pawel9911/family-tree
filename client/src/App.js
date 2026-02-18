afb579b2ee35f64d1302eaee60a88564f2dfffd1
Paweł Gr <pawelgrzybek2@wp.pl>
2026-02-18 13:56:08 +0100

    Add client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hi there</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a href="/auth/google">Sign In With Google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
