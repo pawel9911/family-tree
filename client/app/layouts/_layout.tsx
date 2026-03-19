8af7d908af40705f55656ddddfac756388a96932
Paweł Gr <pawelgrzybek2@wp.pl>
2026-03-19 11:49:09 +0100

    Add header to default layout

--- client/app/layouts/_layout.tsx
+++ client/app/layouts/_layout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router";
+import { Header } from "~/components";
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
+      <div className="container">
+        <Header />
+      </div>
       <main className="flex flex-1">
         <Outlet />
       </main>
