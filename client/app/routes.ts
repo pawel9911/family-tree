9bc73af5b857829561c39d929504ab361cc83092
Paweł Gr <pawelgrzybek2@wp.pl>
2026-03-19 11:48:51 +0100

    Update router to display default layout on not found page

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -6,9 +6,10 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  layout("./layouts/_layout.tsx", [index("./routes/home.tsx")]),
+  layout("./layouts/_layout.tsx", [
+    index("./routes/home.tsx"),
+    route("*", "./routes/not-found.tsx"),
+  ]),
 
   layout("./layouts/auth.tsx", [route("/dashboard", "./routes/dashboard.tsx")]),
-
-  route("*", "./routes/not-found.tsx"),
 ] satisfies RouteConfig;
