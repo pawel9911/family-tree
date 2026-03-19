9b6b83902735108baa431b4be831caba8f210020
Paweł Gr <pawelgrzybek2@wp.pl>
2026-03-19 09:04:20 +0100

    Replace method of components

--- client/app/routes/not-found.tsx
+++ client/app/routes/not-found.tsx
@@ -1,13 +1,15 @@
-export default function NotFound() {
+const NotFound = () => {
   return (
     <div className="container">
       <div className="text-center space-y-6 text-white bg-primary rounded-2xl py-24 px-4">
         <h1 className="text-8xl font-bold">404</h1>
         <p className="text-2xl">The requested page could not be found.</p>
-        <a href="/" className=" underline text-xl">
+        <a href="/" className="underline text-xl">
           Go back to home page
         </a>
       </div>
     </div>
   );
-}
+};
+
+export default NotFound;
