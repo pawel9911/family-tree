3a3cc7a7e1009984d2c84f18800d4fd53ce0203e
Paweł Gr <pawelgrzybek2@wp.pl>
2026-04-15 21:27:20 +0200

    Fix logout on production and optimize middleware order

--- index.js
+++ index.js
@@ -10,16 +10,10 @@ require("./services/passport");
 mongoose.connect(require("./config/keys").mongoURI);
 
 const app = express();
+const isProduction = process.env.NODE_ENV === "production";
 
-if (process.env.NODE_ENV === "production") {
+if (isProduction) {
   app.use(express.static("client/build/client"));
-
-  const path = require("node:path");
-  app.get("/*path", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "build", "client", "index.html"),
-    );
-  });
 }
 
 app.use(express.json());
@@ -34,6 +28,15 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/paymentsRoutes")(app);
+
+if (isProduction) {
+  const path = require("node:path");
+  app.get("/*path", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "client", "index.html"),
+    );
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
