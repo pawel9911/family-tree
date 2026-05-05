b40ae0d8f106036472c5370048c5fd892a19ad95
Paweł Gr <pawelgrzybek2@wp.pl>
2026-05-05 12:21:23 +0200

    Remove PaymentsBanner and related components from Dashboard

--- client/app/pages/dashboard.tsx
+++ client/app/pages/dashboard.tsx
@@ -1,17 +1,5 @@
-import { PaymentsBanner } from "~/features/payments";
-import { Link } from "react-router";
-import { Button } from "~/shared/ui";
-
 const Dashboard = () => {
-  return (
-    <div className="container">
-      <PaymentsBanner>
-        <Button asChild className="mx-auto" size="lg">
-          <Link to="/payments">Zobacz szczegóły</Link>
-        </Button>
-      </PaymentsBanner>
-    </div>
-  );
+  return <div className="container"></div>;
 };
 
 export default Dashboard;
