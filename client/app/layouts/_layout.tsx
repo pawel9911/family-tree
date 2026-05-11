3c1dfe86e878977a13e0a67c756d05c5bebf68fa
Paweł Gr <pawelgrzybek2@wp.pl>
2026-05-11 15:22:47 +0200

    Refactor Layout component to improve scroll behavior and update registration link

--- client/app/layouts/_layout.tsx
+++ client/app/layouts/_layout.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link, Navigate, Outlet } from "react-router";
 import { Header } from "~/components";
 import { useAuthContext } from "~/providers";
 import { Button } from "~/shared/ui";
+import { cn } from "~/utils";
 
 const Layout = () => {
   const { user } = useAuthContext();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () =>
+      setIsScrolled(window.scrollY > window.innerHeight);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   if (user) {
     return <Navigate to="/dashboard" replace />;
@@ -12,10 +23,17 @@ const Layout = () => {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="fixed mt-6 top-6 z-50 w-full">
+      <div
+        className={cn(
+          "fixed top-0 left-0 z-50 w-full transition-all duration-300 ease-in-out",
+          isScrolled
+            ? "translate-y-6 opacity-100"
+            : "-translate-y-full opacity-0",
+        )}
+      >
         <Header className="container">
           <Button size="lg" asChild>
-            <Link to="/login">Dołącz do nas</Link>
+            <Link to="/register">Dołącz do nas</Link>
           </Button>
         </Header>
       </div>
